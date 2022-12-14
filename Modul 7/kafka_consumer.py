3bdd842e683e892f9dd6ffee595bd9f25cefb104
Billie Christian <jjbilliechristian@gmail.com>
2022-12-14 12:49:40 +0700

    added modul 7

---
+++ Modul 7/kafka_consumer.py
@@ -0,0 +1,25 @@
+from ctypes import *
+CDLL(r'C:\Users\Billie\anaconda3\Lib\site-packages\confluent_kafka.libs\librdkafka-09f4f3ec.dll')
+
+from confluent_kafka import Consumer
+################
+c=Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
+print('Kafka Consumer has been initiated...')
+
+print('Available topics to consume: ', c.list_topics().topics)
+c.subscribe(['user-tracker'])
+################
+def main():
+    while True:
+        msg=c.poll(1.0) #timeout
+        if msg is None:
+            continue
+        if msg.error():
+            print('Error: {}'.format(msg.error()))
+            continue
+        data=msg.value().decode('utf-8')
+        print(data)
+    c.close()
+        
+if __name__ == '__main__':
+    main()
